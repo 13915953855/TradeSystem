bc9aa57d6b1cfde8b0a9f9350c4cc87413aae81c
徐森 <xusen@cmiot.chinamobile.com>
2019-04-04 14:16:09 +0800

    get ip

--- src/main/java/com/jason/trade/util/WebSecurityConfig.java
+++ src/main/java/com/jason/trade/util/WebSecurityConfig.java
@@ -54,5 +54,30 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter {
             response.sendRedirect(url);
             return false;
         }
+
+        /**
+         * 获取Ip地址
+         * @param request
+         * @return
+         */
+        public  String getIpAddr(HttpServletRequest request){
+            String ip = request.getHeader("X-Forwarded-For");
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+            }
+            return ip;
+        }
     }
 }
